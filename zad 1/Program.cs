4370ad9c2a1e630c347036b96124b87d03ab85f8
Jivko Karov <jikito2007@gmail.com>
2025-04-08 09:06:26 +0300

    Add project files.

---
+++ zad 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace zad_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            string[] numbersArray = numbers.Split(' ');
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+
+            foreach (string number in numbersArray)
+            {
+                if (int.TryParse(number, out int num))
+                {
+                    uniqueNumbers.Add(num);
+                }
+            }
+
+            Console.WriteLine("The numbers are:");
+            foreach (int number in uniqueNumbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
