832d5527e7ffc23fe5c5009d055e2f55b593fb0e
Jivko Karov <jikito2007@gmail.com>
2025-04-09 12:09:40 +0300

    Задача 3 и 4 са готови!

---
+++ zad 3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace zad_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете текст за извличане на думи:");
+            string[] words;
+            char[] chars = { '!', '?', '.', ';', ',', ' '};
+            words = Console.ReadLine().ToLower().Split(chars);
+
+            HashSet<string> uniqueWords = new HashSet<string>(words);
+
+            Console.WriteLine("Уникалните думи в текста са: ");
+            Console.Write($"{uniqueWords.Count} на брой и са: ");
+            foreach (string word in uniqueWords)
+            {
+                Console.Write(word + ", ");
+            }
+        }
+    }
+}
---
+++ zad 4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            Console.WriteLine("Въведете елементи на първия сет, разделени с интервал: ");
+            int[] ints1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            foreach (int item in ints1)
+            {
+                set1.Add(item);
+            }
+
+            Console.WriteLine("Въведете елементи на първия сет, разделени с интервал: ");
+            int[] ints2 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            foreach (int item in ints2)
+            {
+                set2.Add(item);
+            }
+
+            if (set1.Overlaps(set2))
+            {
+                Console.Write("Двата сета имат общи елементи: ");
+                foreach (int item in set1)
+                {
+                    if (set2.Contains(item))
+                    {
+                        Console.Write(item + ", ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Нямат общи елементи!");
+            }
+        }
+    }
+}
